8dcb505fa952cba887e978b33f045c8905676383
Kris Williams <Kris.Williams@vu.com>
2024-03-14 08:40:18 -0500

    initial commit

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,16 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
+stuff = {"rope": 1, "torch": 6, "gold coin": 42, "ring": 1, "apple": 12}
+
 
 def displayInventory(inventory):
     # your code goes here
+    total = 0
+    print("Inventory:")
+
+    for k, v in inventory.items():
+        print(v, k)
+        total += v
+
+    print("Total number of items:", total)
 
 
 if __name__ == "__main__":
